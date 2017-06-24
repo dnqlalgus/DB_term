00c17fc452dafb80cdf6a299aea3f9f3ae29e8bb
Lee Mi Hyeon <dnqlalgus@gmail.com>
2017-06-24 14:25:16 +0000

    edit list

--- user_main.php
+++ user_main.php
@@ -13,8 +13,6 @@
 	<p style="text-align: center;">환영합니다. <?=$_SESSION['id']?>님</p>
 	<a href="club_view.php"><h3>동아리 목록 보기</h3></a>
 	</br>
-	<a href="club_join.php"><h3>동아리 가입 하기</h3></a>
-	</br>
 	<a href="user_modify.php"><h3>정보 수정</h3></a>
 	</br>
 	<a href="user_delete.php"><h3>회원 탈퇴</h3></a>
