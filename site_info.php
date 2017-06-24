fc6130a16de3eb513cc9aeafef6c9f3e485ba667
Lee Mi Hyeon <dnqlalgus@gmail.com>
2017-06-24 11:37:25 +0000

    site information

---
+++ site_info.php
@@ -0,0 +1,11 @@
+<? include ("header.php"); ?>
+  <!-- Begin page content -->
+  <div class="container">
+    <div class="page-header">
+      <h1>고려대학교 동아리 </h1>
+    </div>
+    <p class="lead">끝내 한길에 하나가 되리 고려대학교 동아리</p>
+  </div>
+</div>
+</div>
+
