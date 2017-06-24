96907dd72fe327920ded7e1a21cfe307f32106d6
Lee Mi Hyeon <dnqlalgus@gmail.com>
2017-06-24 04:16:08 +0000

    change quotation

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8">
 <?php
-    $host = "localhost";
-    $dbid = "root";                    // 자신의 DB ID
-    $dbpass = "linda132";                  // 자신의 DB Password
-    $dbname = "club_db";                  // 해당 프로젝트에서 사용하는 DB명
+    $host = 'localhost';
+    $dbid = 'root';                    // 자신의 DB ID
+    $dbpass = 'linda132';                  // 자신의 DB Password
+    $dbname = 'club_db';                  // 해당 프로젝트에서 사용하는 DB명
     $mysql_port='3307';
     $mysql_charset='utf8';
 ?>
