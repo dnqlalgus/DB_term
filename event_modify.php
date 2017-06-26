5fd45da9b8f402c7a4f72ca9ac6e5d7fd091221b
Lee Mi Hyeon <dnqlalgus@gmail.com>
2017-06-26 14:21:48 +0000

    이벤트 수정

---
+++ event_modify.php
@@ -0,0 +1,27 @@
+<?php
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host,$dbid,$dbpass,$dbname);
+
+$e_id=$_POST['e_id'];
+$c_name=$_POST['c_name'];
+$e_name = $_POST['e_name'];
+$e_place = $_POST['e_place'];
+$e_detail = $_POST['e_detail'];
+
+
+$get = mysql_query("SELECT * from events where e_id='$e_id'", $conn);
+$ret = mysql_query("UPDATE events set c_name = '$c_name', e_name = '$e_name', e_place = '$e_place', e_detail='$e_detail' where e_id = '$e_id'", $conn);
+
+if(!$ret || !$get)
+{
+    msg('Query Error : '.mysql_error($conn));
+}
+else
+{
+    s_msg('성공적으로 수정 되었습니다');
+    echo "<meta http-equiv='refresh' content='0;url=admin_main.php'>";
+}
+
+?>
---
+++ event_modify_form.php
@@ -0,0 +1,45 @@
+<?php
+include "header.php";
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host, $dbid, $dbpass, $dbname);
+$e_id=$_GET['e_id'];
+
+?>
+
+<html>
+<body>
+ <div class="container">
+    <div class="page-header">
+      <h1>이벤트 수정</h1>
+    </div>
+        <form name="e_form" action="event_modify.php" method="post" class="fullwidth">
+            <input type="hidden" name="e_id" value="<?=$e_id?>" >
+
+            <fieldset class="form-group">
+            <label for="c_name">동아리 이름</label>
+            <input type="text" class="form-control" id="c_name" name="c_name" >
+          </fieldset>
+
+          <fieldset class="form-group">
+            <label for="e_name">이벤트 이름</label>
+            <input type="text" class="form-control" id="e_name" name="e_name" >
+          </fieldset>
+
+          <fieldset class="form-group">
+            <label for="e_place">이벤트 장소 및 날짜</label>
+            <input type="text" class="form-control" id="e_place" name="e_place" >
+          </fieldset>
+
+          <fieldset class="form-group">
+            <label for="e_detail">이벤트 설명</label>
+            <textarea class="form-control" id="e_detail" name="e_detail" rows="3"></textarea>
+          </fieldset>
+
+          <p align="center"><button id="submit_btn" class="btn btn-primary" type="submit" value="submit">수정</button></p>
+
+         </form>
+    </div>
+</body>
+</html>
